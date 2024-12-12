f8b54300185f148477762f27b0797a991bb5ec2c
elsonmorina <167892030+elsonmorina@users.noreply.github.com>
2024-12-12 16:16:45 +0100

    day 2

---
+++ day2/index.php
@@ -0,0 +1,45 @@
+
+    <?php
+    $school = "Digital School";
+    echo "i love $school";
+    echo "<br>";
+    echo strlen($school);
+    echo "<br>";
+    echo str_word_count($school);
+    
+    echo "<br>";
+    $programming = "programming is not cool";
+    echo str_replace("not", "very", $programming);
+    echo "<br>";
+    $ei = "digital";
+    $ie = "school";
+    $ja = $ei . $ie;
+    echo "$ja";
+    echo "<br>";
+    $elson = "elson";
+    echo "$elson morina";
+    
+    echo "<br>";
+
+    $x = 20;
+    $y = 3;
+    echo $x + $y;
+    echo "<br>";
+    $a = 30;
+    $b= 10;
+    echo $a / $b;
+   
+    
+    echo "<br>";
+    
+    $c = 12;
+    $d = 6;
+    echo $c * $d;
+    
+    
+    echo "<br>";
+     
+    $e = 45;
+    $f = 5;
+    echo $e % $f;
+    ?>
