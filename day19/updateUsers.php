56c72303b43e0cccef301eca6f0e094f2c77f621
elsonmorina <167892030+elsonmorina@users.noreply.github.com>
2025-03-20 16:16:28 +0100

    day19

---
+++ day19/updateUsers.php
@@ -0,0 +1,26 @@
+<?php
+
+
+    include_once('config.php');
+
+    if(isset($_POST['submit'])){
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+
+        $sql = "UPDATE users SET id=:id, name=:name, username=:username, email=:email WHERE id=:id";
+
+        $prep = $conn->prepare($sql);
+        $prep->bindParam(':id',$id);
+        $prep->bindParam(':name',$name);
+        $prep->bindParam(':username',$username);
+        $prep->bindParam(':email',$email);
+
+        $prep->execute();
+        header('Location:dashboard.php');
+
+    }
+
+
+?>
